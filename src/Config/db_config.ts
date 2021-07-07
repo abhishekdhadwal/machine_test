e8a7c57bdb03034f3159ba6d1beb37b5fa6ca03f
abhishekdhadwal <abhishekdhadwal33@gmail.com>
2021-07-07 17:14:04 +0530

    UPDATED ON JULY 07 2021

--- src/Config/db_config.ts
+++ src/Config/db_config.ts
@@ -7,7 +7,7 @@ let db_config : any = {}
 if(process.env.NODE_ENV == 'local') {
       
       db_config = {
-            PORT : 2030,
+            PORT : process.env.SERVER_PORT_DEV,
             URI : 'mongodb://localhost:27017/base_project_hapi_ts'    
       }
 
@@ -15,7 +15,7 @@ if(process.env.NODE_ENV == 'local') {
 else if(process.env.NODE_ENV == 'server') {
 
       db_config = {
-            PORT : 2030,
+            PORT : process.env.SERVER_PORT_PROD,
             URI : 'mongodb://localhost:27017/base_project_hapi_ts'    
       }
 
